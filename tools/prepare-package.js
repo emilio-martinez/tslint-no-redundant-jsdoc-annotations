0cec414f75a88ec7dedd88efe6ed40110d0014f5
Emilio Martinez-Cordero <e.marticor@gmail.com>
2017-11-01 22:24:24 -0700

    build: add bump on `release` and prevent accidental root publish

--- tools/prepare-package.js
+++ tools/prepare-package.js
@@ -1,15 +1,27 @@
 process.stdin.setEncoding('utf8');
 
-const blacklist = ['scripts', 'devDependencies'];
+/**
+ * These properties will be removed from the package.json on release.
+ * `private` is set on the root package.json to prevent accidental release.
+ */
+const blacklist = ['private', 'scripts', 'scripts-info', 'devDependencies'];
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
+/**
+ * 1) Get and store package.json data
+ * 2) When all the data is received, parse json
+ * 3) Remove blacklisted keys.
+ * 4) Write to stdout
+ */
 let packageJson = '';
-process.stdin.on('data', chunk => {
-  packageJson += chunk;
-});
+
+/** 1 */
+process.stdin.on('data', chunk => { packageJson += chunk });
+
 process.stdin.on('end', () => {
+  /** 2 */
   let parsed;
   try {
     parsed = JSON.parse(packageJson);
@@ -17,12 +29,16 @@ process.stdin.on('end', () => {
     console.error('Cannot parse to JSON');
     process.exit(1);
   }
+
+  /** 3 */
   const result = {};
   Object.keys(parsed).forEach(key => {
     if (blacklist.indexOf(key) < 0) {
       result[key] = parsed[key];
     }
   });
+
+  /** 4 */
   process.stdout.write(JSON.stringify(result, null, 2));
   packageJson = '';
 });
